8518487da2f56789b7ffa03e344b9bac7b46d58a
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-04-15 19:42:49 +0300

    Create p007-10001st-prime.py

---
+++ projecteuler-solutions/p007-10001st-prime.py
@@ -0,0 +1,19 @@
+# Project Euler Problem 7
+# 10001st Prime
+"""
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10 001st prime number?
+"""
+import eulerlib, itertools
+# Computers are fast, so we can implement this solution by testing each number
+# individually for primeness, instead of using the more efficient sieve of Eratosthenes.
+# 
+# The algorithm starts with an infinite stream of incrementing integers starting at 2,
+# filters them to keep only the prime numbers, drops the first 10000 items,
+# and finally returns the first item thereafter.
+def compute():
+	ans = next(itertools.islice(filter(eulerlib.is_prime, itertools.count(2)), 10000, None))
+	return str(ans)
+
+if __name__ == "__main__":
+	print(compute())
