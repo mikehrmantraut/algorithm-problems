a4361e74e5f9edcca59e32a6db3c8ea2c6d92de2
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-04-16 22:54:26 +0300

    Create p010-summation-of-primes.py

---
+++ projecteuler-solutions/p010-summation-of-primes.py
@@ -0,0 +1,14 @@
+# Project Euler Problem 10
+# Summation of Primes 
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.
+"""
+import eulerlib
+
+def comp():
+	ans = sum(eulerlib.primes(1999999))
+	return str(ans)
+
+if __name__ == "__main__":
+	print(comp())
