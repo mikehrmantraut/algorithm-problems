1a06aa3925014b946fd11bcc6cac3da418d16b83
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2023-03-06 13:37:00 +0300

    Create solution.c

---
+++ hackerrank-solutions/C/For Loop in C/solution.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a, b;
+    const char *numbers[9] = {"one", "two", "three", "four",
+    "five", "six", "seven", "eight", "nine"};
+    scanf("%d\n%d", &a, &b);
+    
+    for (int i=a;i<=b;i++)
+    {
+        if ((i>0) && (i<10)) printf("%s\n", numbers[i-1]);
+        else printf("%s\n", (i%2) ? "odd":"even");
+    }
+    return 0;
+}
