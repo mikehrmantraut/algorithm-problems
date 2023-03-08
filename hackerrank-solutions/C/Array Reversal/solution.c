4cd6076fae91ca9b6ba06b0c85509af0acde742a
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2023-03-09 00:57:09 +0300

    Create solution.c

---
+++ hackerrank-solutions/C/Array Reversal/solution.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    while (0 < num) printf("%d ", arr[--num]);
+    return 0;
+}
