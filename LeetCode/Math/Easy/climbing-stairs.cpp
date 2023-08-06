4c0dddcbeb6c37c7bd2cb2c845ee898f125c9598
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2023-08-06 15:56:11 +0300

    Create climbing-stairs.cpp

---
+++ LeetCode/Math/Easy/climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+
+int climbStairs(int n)
+{
+    int a = 1, b = 1, c, i;
+    for (i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+};
